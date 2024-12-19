f70947af184423424899aa844ae26c0aa8b6ae47
phillip deleon <phillipdeleon68@gmail.com>
2024-12-19 13:35:32 -0500

    Add project files.

---
+++ ParkingSystem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_7._4._1
+{
+    public class ParkingSystem
+    {
+        private int[] parkingSpaces;
+      
+
+        public ParkingSystem(int big, int medium, int small)
+        {
+            parkingSpaces = new int[3];
+
+            parkingSpaces[0] = big;
+            parkingSpaces[1] = medium;
+            parkingSpaces[2] = small;
+        }
+        
+        public bool AddCarToLot(int carSize)
+        {
+            if(carSize < 1 ||  carSize > 3 || parkingSpaces[carSize - 1] <= 0 ) //checks for parking space
+            {
+                return false;
+            }
+            parkingSpaces[carSize - 1]--;
+            return true;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Assignment_7._4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ParkingSystem parkingSystem = new ParkingSystem(2, 0, 2); //Open Parking Spots: Big(1): 2 | Meduim(2): 0 | Small(3): 2
+
+            Console.WriteLine(parkingSystem.AddCarToLot(1)); 
+            Console.WriteLine(parkingSystem.AddCarToLot(2)); 
+            Console.WriteLine(parkingSystem.AddCarToLot(3)); 
+            Console.WriteLine(parkingSystem.AddCarToLot(1)); 
+        }
+
+    }
+}
